dc20488fe494b6d081f2ee6825570091c8d7066b
Rickkk-hub <picardal.ricky.roque@gmail.com>
2025-10-04 16:19:55 +0800

    Create Day03.cs

---
+++ Day03.cs
@@ -0,0 +1,23 @@
+//Day 3: Learn variables. Declare string, int, double, bool. Print them.
+using System;
+
+class Day03
+{
+
+
+    public static void Run()
+    {
+        
+        int mynum = 21;
+        double mydouble = 10.99;
+        char myletter = 'R';
+        bool mybool = true;
+        string myString = "rick";
+
+        Console.WriteLine(mynum);
+        Console.WriteLine(mydouble);
+        Console.WriteLine(myletter);
+        Console.WriteLine(mybool);
+        Console.WriteLine(myString);
+    }
+}
